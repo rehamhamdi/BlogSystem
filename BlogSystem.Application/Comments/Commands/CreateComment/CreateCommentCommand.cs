0b5a6027bb739fd54f92eaa1c617632c161eb7ac
Reham Hamdi <rehamhamdi53@gmail.com>
2026-09-05 18:11:18 +0300

    Implemented comment features

---
+++ BlogSystem.Application/Comments/Commands/CreateComment/CreateCommentCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BlogSystem.Application.Comments.Commands.CreateComment;
+
+public record CreateCommentCommand(
+    Guid PostId,
+    string AuthorName,
+    string Text
+) : IRequest<Guid>;
---
+++ BlogSystem.Application/Comments/Commands/CreateComment/CreateCommentHandler.cs
@@ -0,0 +1,34 @@
+﻿using BlogSystem.Application.Interfaces;
+using BlogSystem.Domain.Entities;
+using MediatR;
+
+namespace BlogSystem.Application.Comments.Commands.CreateComment;
+
+public class CreateCommentHandler
+    : IRequestHandler<CreateCommentCommand, Guid>
+{
+    private readonly ICommentRepository _repository;
+
+    public CreateCommentHandler(ICommentRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Guid> Handle(
+        CreateCommentCommand request,
+        CancellationToken ct)
+    {
+        var comment = new Comment
+        {
+            Id = Guid.NewGuid(),
+            PostId = request.PostId,
+            AuthorName = request.AuthorName,
+            Text = request.Text
+        };
+
+        await _repository.AddAsync(comment, ct);
+        await _repository.SaveChangesAsync(ct);
+
+        return comment.Id;
+    }
+}
